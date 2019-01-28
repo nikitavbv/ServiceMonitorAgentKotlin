c57c8c32c07153a7af661146711dfa77dd2a249c
Nikita Volobuev <nikitavbv@gmail.com>
2019-01-28 19:47:16 +0200

    feat(tracking): device io tracking

--- src/linuxMain/kotlin/com/github/nikitavbv/servicemonitor/agent/Tracking.kt
+++ src/linuxMain/kotlin/com/github/nikitavbv/servicemonitor/agent/Tracking.kt
@@ -1,8 +1,11 @@
 package com.github.nikitavbv.servicemonitor.agent
 
 import kotlinx.cinterop.toKString
+import nativeAgent.getCurrentTimeMillis
 import platform.posix.perror
 import nativeAgent.getCurrentTimeRFC3339
+
+val ioPrevState = mutableMapOf<String, Any>()
 
 @ExperimentalUnsignedTypes
 fun runTrackingIteration() {
@@ -55,7 +58,7 @@ fun monitorMemory(params: Map<*, *>): Map<String, Any?> {
     // https://www.kernel.org/doc/Documentation/filesystems/proc.txt
     val result = mutableMapOf<String, Any>()
 
-    readFile("/proc/memFile").lines().forEach { line ->
+    readFile("/proc/meminfo").lines().forEach { line ->
         if (line == "") {
             return@forEach
         }
@@ -77,8 +80,55 @@ fun monitorMemory(params: Map<*, *>): Map<String, Any?> {
 }
 
 fun monitorIO(params: Map<*, *>): Map<String, Any?> {
-    TODO("implement this")
-    return emptyMap()
+    // https://www.kernel.org/doc/Documentation/iostats.txt
+    val result = mutableMapOf<String, Any>()
+    result["devices"] = mutableMapOf<String, Any>()
+
+    readFile("/proc/diskstats").lines().forEach { line ->
+        if (line == "") {
+            return@forEach
+        }
+
+        val fields = line.fields()
+        val deviceName = fields[2]
+        // Block size according to:
+        // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+        // /tree/include/linux/types.h?id=v4.4-rc6#n121
+        val deviceBlockSize = 512
+        val sectorsRead = fields[5].toLong()
+        val sectorsWritten = fields[6].toLong()
+        val timestamp = getCurrentTimeMillis()
+        if (deviceName.startsWith("loop")) {
+            return@forEach
+        }
+
+        val devicePrevState = ioPrevState[deviceName] as Map<*, *>?
+        if (devicePrevState != null) {
+            val prevSectorsRead = devicePrevState["sectorsRead"] as Long
+            val prevSectorsWritten = devicePrevState["sectorsWritten"] as Long
+            val prevTimestamp = devicePrevState["timestamp"] as Long
+            val bytesRead = (sectorsRead - prevSectorsRead) * deviceBlockSize
+            val bytesWritten = (sectorsWritten - prevSectorsWritten) * deviceBlockSize
+            val bytesReadPerSecond = (bytesRead) / ((timestamp - prevTimestamp) / 1000)
+            val bytesWrittenPerSecond = (bytesWritten) / ((timestamp - prevTimestamp) / 1000)
+            (result["devices"] as MutableList<*>).add(mapOf(
+                "device" to deviceName,
+                "read" to bytesReadPerSecond,
+                "write" to bytesWrittenPerSecond
+            ))
+        }
+        ioPrevState[deviceName] = mapOf(
+            "sectorsRead" to sectorsRead,
+            "sectorsWritten" to sectorsWritten,
+            "timestamp" to timestamp
+        )
+    }
+
+    if ((result["devices"] as List<*>).size == 0) {
+        return emptyMap()
+    }
+
+    return result
 }
 
 fun monitorDiskUsage(params: Map<*, *>): Map<String, Any?> {
